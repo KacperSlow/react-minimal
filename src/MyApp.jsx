27de80e4a1fe22e545d1f603d94e52d96631891d
KacperSlow <k.slowikowski@student.mini.pw.edu.pl>
2019-10-12 19:30:02 +0200

    Lab1Ex2

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -9,5 +9,7 @@ const MyApp = () => (
     <p>Bundle size: 58 bytes, Load time of the bundle: 33 ms, Last commit SHA1: 789578f9292d927e88037898bcd274519ce4a3bf</p>
   </div>
 )
+const generateArray = (n) =>Array.from(Array(n), (x,index)=>index+1)
+console.log(generateArray(30))
 
 export default MyApp
