29503dcd37f8200930a623a95206b2cace0e93b1
KacperSlow <k.slowikowski@student.mini.pw.edu.pl>
2019-10-12 15:13:13 +0200

    Production bundle analysis

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const MyApp = () => (
-  <h1>Minimal React KacperSlow
-    <br/>
-    
-  </h1> 
+  <div>
+    <h1>Minimal React KacperSlow</h1> 
+    <p>Bundle size: 58 bytes, Load time of the bundle: 33 ms, Last commit SHA1: 789578f9292d927e88037898bcd274519ce4a3bf</p>
+  </div>
   
 
 )
